d9b9e149d0c618b813904e19ff45563730fd7763
Ezau Martins <60906775+EzauMartins@users.noreply.github.com>
2022-04-11 20:02:45 -0400

    Update ContaEspecial.java

--- src/ContaEspecial.java
+++ src/ContaEspecial.java
@@ -1,9 +1,20 @@
 
 public class ContaEspecial {
 
+<<<<<<< Updated upstream
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+=======
+	double limiteAdicional;
+	double tax = 5.0;
+qq
+	public ContaEspecial(String numConta, double saldo, double limiteAdicional, String numCartao) {
+		this.numConta = numConta;
+		this.saldo = saldo;
+		this.limiteAdicional = limiteAdicional;
+		this.numCartao = numCartao;
+>>>>>>> Stashed changes
 	}
 
 }
