97d56a81519ebbf5c0e239ea5856005748e21dcd
Ezau Martins <60906775+EzauMartins@users.noreply.github.com>
2022-04-18 20:31:27 -0400

    update controller e service de Contacorrente
    GET POST DELETE

--- src/main/java/com/wipro/gama/bankapp/service/ContaCorrenteService.java
+++ src/main/java/com/wipro/gama/bankapp/service/ContaCorrenteService.java
@@ -1,12 +1,10 @@
 package com.wipro.gama.bankapp.service;
 
-import com.wipro.gama.bankapp.controller.ContaCorrenteController;
 import com.wipro.gama.bankapp.model.ContaCorrente;
 import com.wipro.gama.bankapp.repository.ContaCorrenteRepository;
-import com.wipro.gama.bankapp.repository.ContaEspecialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,36 +14,23 @@ public class ContaCorrenteService {
     @Autowired
     ContaCorrenteRepository repository;
 
-
-    public ContaCorrente findById(Integer id){   // LISTAR POR ID
+    public ContaCorrente findById(Integer id) {   // LISTAR POR ID
         Optional<ContaCorrente> CC = repository.findById(id);
         return CC.orElse(null);
     }
 
-    public List<ContaCorrente> findAll(){       // Retorna Todos
+    public List<ContaCorrente> findAll() {      // Retorna Todos
         return repository.findAll();
     }
 
-
-  /*
-
-    public Clientes update(Integer id, Clientes obj) {
-        Clientes newObj = findById(id);
-        newObj.setNome(obj.getNome());
-        newObj.setSobrenome(obj.getSobrenome());
-        newObj.setEmail(obj.getEmail());
-        newObj.setSenha(obj.getSenha());
-        return repository.save(newObj);
+    public ContaCorrente create(ContaCorrente CC) {
+        return repository.save(CC);
     }
 
-    public Clientes create(Clientes obj) {
-        return repository.save(obj);
-    }
+    //FAZER UPDATE
 
     public void delete(Integer id) {
-        findById(id);
         repository.deleteById(id);
-    }*/
-
-
+    }
 }
+
