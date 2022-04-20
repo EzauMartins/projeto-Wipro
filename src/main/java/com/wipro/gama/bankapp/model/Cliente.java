a87f7798a5912c06c64a465e89e8882e8d141ffa
Ivanildo Ribeiro <75269330+IvanRibeiro011@users.noreply.github.com>
2022-04-20 10:50:09 -0300

    Método update realizado

--- src/main/java/com/wipro/gama/bankapp/model/Cliente.java
+++ src/main/java/com/wipro/gama/bankapp/model/Cliente.java
@@ -1,14 +1,20 @@
 package com.wipro.gama.bankapp.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.*;
-import javax.validation.constraints.Size;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 
 @Entity
