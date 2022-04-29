26e9c737aaffad5dbaa7033d9f638b7432668fb9
Ezau Martins <60906775+EzauMartins@users.noreply.github.com>
2022-04-28 20:22:57 -0400

    Update SwaggerConfig.java

--- src/main/java/com/wipro/gama/bankapp/config/SwaggerConfig.java
+++ src/main/java/com/wipro/gama/bankapp/config/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
     public Docket client() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.wipro.gama.bankapp"))
+                .apis(RequestHandlerSelectors.basePackage("com.wipro.gama.bankapp.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaInfo());
